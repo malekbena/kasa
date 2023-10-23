e0984c4da531c38dd4ec65bfab6b133290eeed6b
malekbena <malekb.benali@gmail.com>
2023-10-23 17:03:21 +0200

    git init

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
-
+// const router = createBrowserRouter({
+//   path: "/",
+//   element: <Home />,
+// })
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
 
