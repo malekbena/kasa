bf2965d029f80f06f8c3879866965961356ba90a
malekbena <malekb.benali@gmail.com>
2023-10-30 12:42:22 +0100

    header navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,24 @@
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
+
 const Navbar = () => {
     return (
         <header>
+            <NavLink to={"/"}>
+                <img src={logo} alt="logo Kasa" />
+            </NavLink>
             <nav>
                 <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
+                    <li>
+                        <NavLink to={"/"} className={(nav)=> nav.isActive ? "active" : ""}>
+                            accueil
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={"/about"} className={(nav)=> nav.isActive ? "active" : ""}>
+                            a propos
+                        </NavLink>
+                    </li>
                 </ul>
             </nav>
         </header>
