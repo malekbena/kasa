c6fea282463d1f9eceadfdb919b08396c577944b
malekbena <malekb.benali@gmail.com>
2023-10-31 14:47:14 +0100

    About page and collapse component desktop version

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,22 @@
+import Banner from "../components/Banner"
+import Collapse from "../components/Collapse";
+
+import aboutBanner from "../assets/montains.png"
+import data from "../data/about.json"
+
 const About = () => {
     return (
-        <div>
-            About Page
-        </div>
+        <>
+            <Banner img={aboutBanner} />
+            <div className="about">
+                {
+                    data &&
+                    data.map((data, index) => (
+                        <Collapse key={index} title={data.title} desc={data.description} />
+                    ))
+                }
+            </div>
+        </>
     );
 }
 export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import Banner from "../components/Banner"
 import Cards from "../components/Cards";
 
-import datas from "../datas/datas.json"
+import data from "../data/data.json"
 import homeBanner from "../assets/beach.png"
 
 const Home = () => {
   return (
     <>
       <Banner img={homeBanner} txt={"Chez vous, partout et ailleurs"} />
-      <Cards housings={datas} />
+      <Cards housings={data} />
     </>
   );
 }
