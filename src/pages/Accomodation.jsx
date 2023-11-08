4134f70353528561002fc6f5662f488e9fee3b4f
malekbena <malekb.benali@gmail.com>
2023-11-08 18:45:54 +0100

    Accomodation tags, host and rating base code

--- src/pages/Accomodation.jsx
+++ src/pages/Accomodation.jsx
@@ -1,14 +1,40 @@
 import { useParams } from "react-router-dom";
 import data from "../data/data.json";
 import Carousel from "../components/Carousel";
+import Tag from "../components/Tag";
+import Rating from "../components/Rating";
 
 const Accomodation = () => {
   const { id } = useParams();
   const accomodation = data.find((housing) => housing.id === id);
   return (
-    <div>
+    <div className="accomodation">
       <Carousel data={accomodation.pictures} />
-
+      <div className="accomodation_box">
+        <div className="accomodation_block1">
+          <h2> {accomodation.title} </h2>
+          <p> {accomodation.location} </p>
+          {
+            accomodation.tags && (
+              <div className="tags">
+                {
+                  accomodation.tags.map((tag, index) => (
+                    <Tag key={index} tag={tag} />
+                  ))
+                  
+              }  
+              </div>
+            )
+          }
+        </div>
+        <div className="accomodation_block2">
+          <div className="accomodation_host">
+            <p> {accomodation.host.name} </p>
+            <img src={accomodation.host.picture} alt="" />
+          </div>
+          <Rating rating={accomodation.rating} />  
+        </div>
+      </div>
     </div>
   );
 }
