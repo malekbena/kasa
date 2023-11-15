2e4ec25a1f22f24facf579df1ef1582c8d248080
malekbena <malekb.benali@gmail.com>
2023-11-15 13:31:03 +0100

    404 page responsive

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -4,7 +4,7 @@ const Error404 = () => {
     return (
         <div className="error404">
             <h1>404</h1>
-            <p>Oups! La page que vous demandez n'existe pas</p>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
             <NavLink to="/">Retour sur la page d'accueil</NavLink>
         </div>
     );
