2bf01ea2a791606489b3a1ca8b938c6c33a4860b
malekbena <malekb.benali@gmail.com>
2023-11-23 16:24:46 +0100

    destructuring carousel props

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,24 +2,24 @@ import { useState } from 'react'
 import arrow_left from '../assets/arrow_left.svg'
 import arrow_right from '../assets/arrow_right.svg'
 
-const Carousel = (data) => {
+const Carousel = ({data}) => {
     const [current, setCurrent] = useState(0)
     const nextSlide = (e) => {
         e.preventDefault()
-        setCurrent(current === data.data.length - 1 ? 0 : current + 1)
+        setCurrent(current === data.length - 1 ? 0 : current + 1)
     }
     const prevSlide = (e) => {
         e.preventDefault()
-        setCurrent(current === 0 ? data.data.length - 1 : current - 1)
+        setCurrent(current === 0 ? data.length - 1 : current - 1)
     }
     return (
         <>
             {
-                data.data &&
+                data &&
                 <div className="carousel">
-                    <img className='cover' src={data.data[current]} alt="" />
+                    <img className='cover' src={data[current]} alt="" />
                     {
-                        data.data.length > 1 &&
+                        data.length > 1 &&
                         <>
                             <button id='carouselRight' onClick={e => { nextSlide(e) }}>
                                 <img src={arrow_right} alt="arrow right" />
@@ -28,7 +28,7 @@ const Carousel = (data) => {
                                 <img src={arrow_left} alt="arrow left" />
                             </button>
                             <div className='carousel_count'>
-                                {current + 1}/{data.data.length}
+                                {current + 1}/{data.length}
                             </div>
                         </>
                     }
